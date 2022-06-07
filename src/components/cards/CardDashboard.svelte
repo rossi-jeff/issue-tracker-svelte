52a3bda751c24988f4782b50a8d73f266e7d19ae
Jeff Rossi <rossi.jeff@gmail.com>
2022-06-07 11:17:50 -0400

    drag and drop on dashboard

--- src/components/cards/CardDashboard.svelte
+++ src/components/cards/CardDashboard.svelte
@@ -1,76 +1,93 @@
 <script>
-  import Card, { Content as CardContent } from '@smui/card';
-  import Accordion, { Panel, Header, Content } from '@smui-extra/accordion';
-  import { FormatName } from '../../lib';
-  export /**
-* @type {{ SequenceNumber: any; Title: any; Details: any; Project: { Name: any; }; Priority: any; Status: any; Type: any; Complexity: any; Created: any; Author: { Name: { First: any; Middle: any; Last: any; }; }; AssignedTo: { Name: { First: any; Middle: any; Last: any; }; }; }}
-*/
-   let issue
-   let visible = false
+	import Card, { Content as CardContent } from '@smui/card';
+	import Accordion, { Panel, Header, Content } from '@smui-extra/accordion';
+	import { FormatName } from '../../lib';
+  import { onMount } from 'svelte'
+	export /**
+	 * @type {{ UUID: string | null | undefined; SequenceNumber: any; Title: any; Details: any; Project: { Name: any; }; Priority: any; Status: any; Type: any; Complexity: any; Created: any; Author: { Name: { First: any; Middle: any; Last: any; }; }; AssignedTo: { Name: { First: any; Middle: any; Last: any; }; }; }}
+	 */
+	let issue;
+	export /**
+	 * @type {boolean}
+	 */
+	let draggable;
+	let visible = false;
 
-   const toggleVisible = () => {
-     visible = !visible
-   }
+  export let enableDrag = (/** @type {any} */ UUID) => {}
+
+	const toggleVisible = () => {
+		visible = !visible;
+	};
+
+  onMount(() => {
+    if (draggable) enableDrag(issue.UUID);
+  });
 </script>
 
-<Card variant="outlined" style="margin-bottom: 0.25em">
-  <CardContent>
-    <div on:click={toggleVisible} class="dashboard-header">
-      {issue.SequenceNumber}
-    </div>
-    {#if visible}
-      <div style="margin-bottom: 0.5em">{issue.Title}</div>
-      <Accordion>
-        <Panel>
-          <Header>Description</Header>
-          <Content>
-            {issue.Details}
-          </Content>
-        </Panel>
-        <Panel>
-          <Header>Details</Header>
-          <Content>
-            {#if issue.Project}
-            <div>
-              <strong>Project</strong>
-              {issue.Project.Name}
-            </div>
-            {/if}
-            <div>
-              <strong>Priority</strong>
-              {issue.Priority}
-            </div>
-            <div>
-              <strong>Status</strong>
-              {issue.Status}
-            </div>
-            <div>
-              <strong>Type</strong>
-              {issue.Type}
-            </div>
-            <div>
-              <strong>Complexity</strong>
-              {issue.Complexity}
-            </div>
-            <div>
-              <strong>Created</strong>
-              {issue.Created}
-            </div>
-            {#if issue.Author}
-            <div>
-              <strong>Author</strong>
-              {FormatName(issue.Author.Name)}
-            </div>
-            {/if}
-            {#if issue.AssignedTo}
-            <div>
-              <strong>Assigned</strong>
-              {FormatName(issue.AssignedTo.Name)}
-            </div>
-            {/if}
-          </Content>
-        </Panel>
-      </Accordion>
-    {/if}
-  </CardContent>
+<Card
+	variant="outlined"
+	style="margin-bottom: 0.25em"
+	{draggable}
+	class="dashboard-card"
+	id={issue.UUID}
+>
+	<CardContent>
+		<div on:click={toggleVisible} class="dashboard-header">
+			{issue.SequenceNumber}
+		</div>
+		{#if visible}
+			<div style="margin-bottom: 0.5em">{issue.Title}</div>
+			<Accordion>
+				<Panel>
+					<Header>Description</Header>
+					<Content>
+						{issue.Details}
+					</Content>
+				</Panel>
+				<Panel>
+					<Header>Details</Header>
+					<Content>
+						{#if issue.Project}
+							<div>
+								<strong>Project</strong>
+								{issue.Project.Name}
+							</div>
+						{/if}
+						<div>
+							<strong>Priority</strong>
+							{issue.Priority}
+						</div>
+						<div>
+							<strong>Status</strong>
+							{issue.Status}
+						</div>
+						<div>
+							<strong>Type</strong>
+							{issue.Type}
+						</div>
+						<div>
+							<strong>Complexity</strong>
+							{issue.Complexity}
+						</div>
+						<div>
+							<strong>Created</strong>
+							{issue.Created}
+						</div>
+						{#if issue.Author}
+							<div>
+								<strong>Author</strong>
+								{FormatName(issue.Author.Name)}
+							</div>
+						{/if}
+						{#if issue.AssignedTo}
+							<div>
+								<strong>Assigned</strong>
+								{FormatName(issue.AssignedTo.Name)}
+							</div>
+						{/if}
+					</Content>
+				</Panel>
+			</Accordion>
+		{/if}
+	</CardContent>
 </Card>
--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { crumbs, progress, baseUrl, clone } from '../lib';
+	import { crumbs, progress, baseUrl, clone, session } from '../lib';
 	import { onMount } from 'svelte';
 	import LayoutGrid, { Cell } from '@smui/layout-grid';
 	import { CardDashboard } from '../components/cards';
@@ -12,15 +12,80 @@
 	 */
 	let issues = [];
 	/**
-* @type {{ new: any[]; accepted: any[]; assigned: any[]; fixed: any[]; other: any[]; }}
-*/
+	 * @type {{ new: any[]; accepted: any[]; assigned: any[]; fixed: any[]; other: any[]; }}
+	 */
 	let sorted = {
 		new: [],
 		accepted: [],
 		assigned: [],
 		fixed: [],
 		other: []
+	};
+
+	const setupDroppable = () => {
+		const droppables = document.querySelectorAll('.drop-target');
+		droppables.forEach((d) => {
+			d.addEventListener('dragenter', dragEnter);
+			d.addEventListener('dragover', dragOver);
+			d.addEventListener('dragleave', dragExit);
+			d.addEventListener('drop', drop);
+		});
+	};
+
+	const dragStart = (/** @type {any} */ e) => {
+		e.dataTransfer.setData('text/plain', e.target.id);
+	};
+
+	const dragging = (/** @type {any} */ e) => {
+		e.dataTransfer.setData('text/plain', e.target.id);
+	};
+
+	const enableDrag = (/** @type {any} */ UUID) => {
+		const draggable = document.getElementById(UUID)
+		if (draggable) {
+			draggable.addEventListener('dragstart', dragStart)
+			draggable.addEventListener('drag', dragging)
+		}
 	}
+
+	const dragOver = (/** @type {any} */ e) => {
+		e.preventDefault();
+		e.target.classList.add('drag-over');
+	};
+
+	const dragEnter = (/** @type {any} */ e) => {
+		e.preventDefault();
+		e.target.classList.add('drag-over');
+	};
+
+	const dragExit = (/** @type {any} */ e) => {
+		e.target.classList.remove('drag-over');
+	};
+
+	const drop = (/** @type {any} */ e) => {
+		console.log('drop', e);
+		e.target.classList.remove('drag-over');
+
+		const id = e.dataTransfer.getData('text/plain');
+		let parent, target = e.target;
+		while (target.classList && !target.classList.contains('drop-target')) {
+			parent = target.parentNode;
+			if (parent) target = parent
+		}
+		if (id && target) {
+			const draggable = document.getElementById(id)
+			target.appendChild(draggable)
+		}
+		console.log(`Dropped: ${id} in ${target.id}`);
+	};
+
+	/**
+	 * @type {{ signedIn: any; Name?: string; UserName?: string; Token?: string; UUID?: string; SessionId?: string; }}
+	 */
+	let currentUser;
+	session.subscribe((value) => {
+		currentUser = value;
+	});
 
 	onMount(() => {
 		fetch(`${baseUrl}/issue`)
@@ -46,8 +111,11 @@
 							break;
 					}
 				}
-				sorted = clone(sorted)
+				sorted = clone(sorted);
 				progress.set(false);
+				if (currentUser.signedIn) {
+					setupDroppable();
+				}
 			});
 	});
 </script>
@@ -55,43 +123,43 @@
 <LayoutGrid>
 	<Cell span={3}>
 		<h4>New</h4>
-		<div class="auto-300">
+		<div class="auto-300 drop-target" id="drop-new">
 			{#each sorted.new as issue (issue.Id)}
-				<CardDashboard {issue} />
+				<CardDashboard {issue} draggable={currentUser.signedIn} {enableDrag} />
 			{/each}
 		</div>
 	</Cell>
 	<Cell span={3}>
 		<h4>Assigned</h4>
-		<div class="auto-300">
+		<div class="auto-300 drop-target" id="drop-assigned">
 			{#each sorted.assigned as issue (issue.Id)}
-				<CardDashboard {issue} />
+				<CardDashboard {issue} draggable={currentUser.signedIn} {enableDrag} />
 			{/each}
 		</div>
 	</Cell>
 	<Cell span={3}>
 		<h4>Accepted</h4>
-		<div class="auto-300">
+		<div class="auto-300 drop-target" id="drop-accepted">
 			{#each sorted.accepted as issue (issue.Id)}
-				<CardDashboard {issue} />
+				<CardDashboard {issue} draggable={currentUser.signedIn} {enableDrag} />
 			{/each}
 		</div>
 	</Cell>
 	<Cell span={3}>
 		<h4>Fixed</h4>
-		<div class="auto-300">
+		<div class="auto-300 drop-target" id="drop-fixed">
 			{#each sorted.fixed as issue (issue.Id)}
-				<CardDashboard {issue} />
+				<CardDashboard {issue} draggable={currentUser.signedIn} {enableDrag} />
 			{/each}
 		</div>
 	</Cell>
 </LayoutGrid>
 
-<div>
+<div style="padding: 1em">
 	<h4>Other</h4>
-	<div class="auto-300">
+	<div class="auto-300 drop-target" id="drop-other">
 		{#each sorted.other as issue (issue.Id)}
-			<CardDashboard {issue} />
+			<CardDashboard {issue} draggable={false} enableDrag={() => {return false}} />
 		{/each}
 	</div>
 </div>
